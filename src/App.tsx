8b825928c0a2db346f6e357d314234c04c774e78
Wiput Pootong <wiput.pootong@gmail.com>
2019-08-03 21:20:29 +0700

    :sparkles: add spacer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import space from './spacer'
 
 const App: React.FC = () => {
+  const [text, setText] = useState('')
+
+  const handleChange = (sentence: string) => {
+    setText(space(sentence))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <h1>S p a c e i t</h1>
+      </div>
+      <div className="row">
+        <input type="text" onChange={e => handleChange(e.target.value)} />
+      </div>
+      <div className="row">
+        <input type="text" value={text} disabled={true} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/spacer.ts
@@ -0,0 +1,42 @@
+function isThaiVowels(char: string): boolean {
+  const vowels = ['่', '้', '๊', '๋', 'ิ', 'ี', 'ึ', 'ื', '็', 'ั', 'ุ', 'ู']
+
+  if (vowels.includes(char)) {
+    return true
+  }
+  return false
+}
+
+function space(sentence: string): string {
+  let result = ''
+
+  sentence.split('').forEach(char => {
+    if (char === ' ') {
+      return
+    }
+
+    if (isThaiVowels(char)) {
+      if (result[result.length - 1] === ' ') {
+        result = result.slice(0, -1)
+      }
+
+      result += char + ' '
+      return
+    }
+
+    if (char === 'ำ') {
+      if (result[result.length - 1] === ' ') {
+        result = result.slice(0, -1)
+      }
+
+      result += 'ํ า '
+      return
+    }
+
+    result += char + ' '
+  })
+
+  return result
+}
+
+export default space
